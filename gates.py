7caa028922548c1557a5669246c275551c6fa0e5
Hemaraj Dhakal <np05cp4a190064@iic.edu.np>
2021-01-26 12:42:53 +0545

    Logic Gates Module

---
+++ gates.py
@@ -0,0 +1,9 @@
+#Gates according our 8 bit model
+def orGate (b1,b2): #or gate
+    return b1|b2
+
+def andGate (b1,b2): #and gate
+    return b1&b2
+
+def xorGate (b1,b2): #xor gate
+    return b1^b2
